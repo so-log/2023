cc4ff04a9e16a64c0c13a7334fedd9299d91a1ed
so <ansso8430@gmail.com>
2023-04-17 17:55:04 +0900

    type function

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ function add(n1, n2) {
     return n1 + n2;
 }
 function printResult(num) {
-    console.log('Result: ' + num);
+    console.log("Result: " + num);
 }
-printResult(add(15, 2));
+printResult(add(15, 2)); // Result: 17
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
 function add(n1: number, n2: number) {
-    return n1 + n2;
+  return n1 + n2;
 }
 
-function printResult(num: number) : void {
-    console.log('Result: ' + num);
+function printResult(num: number): void {
+  console.log("Result: " + num);
 }
 
-printResult(add(15, 2));    // Result: 17
+printResult(add(15, 2)); // Result: 17
+
+let combineValues: (a: number, b: number) => number;
+
+// combineValues = 5;
+// combineValues = printResult;
+combineValues = add;
+
+console.log(combineValues(8, 8));
