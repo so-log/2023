232fea59bf5ad9f6215b7bcb2bb97f719774a5c3
so <ansso8430@gmail.com>
2023-04-18 17:11:47 +0900

    unknown, never type

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "so";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(15, 2)); // Result: 17
-var combineValues;
-// combineValues = 5;
-// combineValues = printResult;
-combineValues = add;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+generateError("An error occurred", 50);
--- app.ts
+++ app.ts
@@ -1,27 +1,15 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "so";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
 
-function printResult(num: number): void {
-  console.log("Result: " + num);
+function generateError(message: string, code: number) {
+  throw { message: message, errorCode: code };
 }
 
-function addAndHandle(n1: number, n2: number, cb: (nb: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(15, 2)); // Result: 17
-
-let combineValues: (a: number, b: number) => number;
-
-// combineValues = 5;
-// combineValues = printResult;
-combineValues = add;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-  return result;
-});
+generateError("An error occurred", 500);
