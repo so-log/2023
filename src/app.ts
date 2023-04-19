2c93a35410884785a13660561b3958269a91db6b
so <ansso8430@gmail.com>
2023-04-19 20:17:23 +0900

    javascript

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,65 @@
-const button = document.querySelector("button")!;
+// const userName = "so";
+// userName = "an";
+// let age = 30;
 
-function clickHandler(message: string) {
-  console.log("Clicked " + message);
-}
+// age = 29;
+
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
+
+// if (age > 20) {
+//   let isOld = true;
+// }
+
+// console.log(isOld); // let, const 모두 block내에서만 유효
+
+// console.log(result);
+
+const add = (a: number, b: number = 1) => a + b;
+
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output);
+
+const button = document.querySelector("button");
 
 if (button) {
-  button.addEventListener("click", clickHandler.bind(null, "You're welcome"));
+  button.addEventListener("click", (event) => console.log(event));
 }
+
+printOutput(add(5));
+
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: "so",
+  age: 20,
+};
+
+const copiedPerson = { ...person };
+
+// const add = (...numbers: number[]) => {
+// const add = (...numbers: [number, number, number, number]) => {
+//   return numbers.reduce((curResult, curValue) => {
+//     return curResult + curValue;
+//   }, 0);
+// };
+
+// const addedNumbers = add(5, 10, 2, 3.7);
+// console.log(addedNumbers);
+
+// const hobby1 = hobbies[0];
+// const hobby2 = hobbies[1];
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age);
