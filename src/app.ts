9a60daf83351760b57114a812ce1b278b0b2fd0a
so <ansso8430@gmail.com>
2023-05-08 17:46:56 +0900

    typeof, type guard

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,76 @@
-class Department {
+type Admin = {
   name: string;
+  privileges: string[];
+};
 
-  constructor(n: string) {
-    this.name = n;
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// interface ElevatedEmployee extends Employee, Admin {}
+
+type ElevatedEmployee = Admin & Employee;
+
+const el: ElevatedEmployee = {
+  name: "so",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  // type guard
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
   }
+  return a + b;
+}
 
-  describe() {
-    console.log("Department " + this.name);
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("StartDate: " + emp.startDate);
   }
 }
 
-const accounting = new Department("Accounting");
+printEmployeeInformation({ name: "so", startDate: new Date() });
 
-accounting.describe();
+class Car {
+  drive() {
+    console.log("Driving");
+  }
+}
 
-const accountingCopy = { describe: accounting.describe };
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading cargo..." + amount);
+  }
+}
 
-accountingCopy.describe();
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
