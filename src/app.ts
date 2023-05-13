4b14afbf9bf97c46f8901c6633ace219111174c7
so <ansso8430@gmail.com>
2023-05-13 15:14:06 +0900

    extends

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,38 @@
 // const names = ["so", "hi"];
 // generic type
-const names: Array<string> = []; // string[]
+// const names: Array<string> = []; // string[]
 
-// promise type
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve("This is done");
-    resolve(10);
-  }, 2000);
-});
+// // promise type
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve("This is done");
+//     resolve(10);
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  //   data.split(''); // ERR!!
-});
+// promise.then((data) => {
+//   //   data.split(''); // ERR!!
+// });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "so", hobbies: ["Sports"] }, { age: 22 });
+console.log(mergedObj.name);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 value";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
