5d35f284789fcbaa61854a5181f56011ba0aefb5
so <ansso8430@gmail.com>
2023-05-25 17:48:38 +0900

    mock service

---
+++ fetch/src/mocks/handlers.js
@@ -0,0 +1,40 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.post("/login", (req, res, ctx) => {
+    return res(
+      ctx.json({
+        id: "f79e82e8-c34a-4dc7-a49e-9fadc0979fda",
+        firstName: "John",
+        lastName: "Maverick",
+      })
+    );
+  }),
+  rest.get(
+    "https://raw.githubusercontent.com/techoi/raw-data-api/main/simple-api.json",
+    async (req, res, ctx) => {
+      const id = req.url.searchParams.get("id");
+
+      const originalResponse = await ctx.fetch(req);
+      const originalResponseData = await originalResponse.json();
+
+      return res(
+        // ctx.status(500),
+        // ctx.json({
+        //   errorMessage: `Data not found`,
+        // })
+        ctx.json({
+          data: {
+            people: [
+              ...originalResponseData.data.people,
+              {
+                name: id,
+                age: 135,
+              },
+            ],
+          },
+        })
+      );
+    }
+  ),
+];
