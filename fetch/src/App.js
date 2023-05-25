de7b7015bbadd139418128dae9d32047ea305b45
so <ansso8430@gmail.com>
2023-05-25 21:27:29 +0900

    react-redux

--- fetch/src/App.js
+++ fetch/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import { TestMocking } from "./components/TestMocking.tsx";
+import Counter from "./features/counter/Counter";
+import Counter2 from "./components/Counter2";
 
 function App() {
   return (
     <div className="App">
       <TestMocking />
+      <Counter />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Counter2 />
     </div>
   );
 }
