bd375314aaec4cd7f0009a195d8bdd0054b8be1b
so <ansso8430@gmail.com>
2023-05-26 16:34:23 +0900

    recoil

--- fetch/src/App.js
+++ fetch/src/App.js
@@ -2,18 +2,29 @@ import "./App.css";
 import { TestMocking } from "./components/TestMocking.tsx";
 import Counter from "./features/counter/Counter";
 import Counter2 from "./components/Counter2";
+import { RecoilRoot } from "recoil";
+import { FontButton } from "./components/RecoilExample/FontButton.jsx";
+import { Text } from "./components/RecoilExample/Text.jsx";
+import { CharacterCounter } from "./components/RecoilExample/CharactorCounter";
+import { TodoList } from "./components/RecoilExample/Todo/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <TestMocking />
+      {/* <TestMocking />
       <Counter />
       <br />
       <br />
       <br />
       <br />
       <br />
-      <Counter2 />
+      <Counter2 /> */}
+      <RecoilRoot>
+        {/* <FontButton />
+        <Text />
+        <CharacterCounter /> */}
+        <TodoList />
+      </RecoilRoot>
     </div>
   );
 }
